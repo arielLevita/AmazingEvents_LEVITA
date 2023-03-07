961dce2ec97b00db090e3f587314b7112df61a99
arielLevita <levita.ariel@gmail.com>
2023-03-07 11:22:43 -0300

    Entrega Task 2 (+ eventos de selección de categorías).

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,12 @@
 import data from "./amazing.js";
 
 
-// Crear las tarjetas.
-// En checkedEvents, cambiar visible o hidden. Ver cómo se hace desde el DOM.
-// Ver cómo se hace delete div desde el DOM.  --> sólo para saber.
-
-
-
-
 // Guardar la información de amazing.js en una variable para acceder y manipular la información.
 let events = data.events;
 
+
 // Recoger los input de los checkboxes de selector de categorías.
-let checkFoodFair = document.getElementById("catFoodFair");
+export let checkFoodFair = document.getElementById("catFoodFair");
 let checkMuseum = document.getElementById("catMuseum");
 let checkCostumeParty = document.getElementById("catCostumeParty");
 let checkMusicConcert = document.getElementById("catMusicConcert");
@@ -20,35 +14,33 @@ let checkRace = document.getElementById("catRace");
 let checkBookExchange = document.getElementById("catBookExchange");
 let checkCinema = document.getElementById("catCinema");
 
-// Almacenar en una variable el div en el que van a estar las tarjetas (para posicionarlas).
-let cardsSection = document.getElementById("cards");
 
 // Crear las tarjetas dentro del div "cards". Asignarles la imagen de fondo.
-for (let i = 0; i < events.length; i++) {
-    let div =
-        document.createElement("div");
-        div.classList.add('card');
-        div.classList.add('d-flex');
-        div.classList.add('flex-column');
-        div.setAttribute('id', `card${events[i]._id}`);
+function cardGenerator(eventsArray) {
+    let dFrag = document.createDocumentFragment();
+    for (let i = 0; i < eventsArray.length; i++) {
+        let div = document.createElement("div");
+        div.classList.add('card', 'd-flex', 'flex-column');
+        div.setAttribute('id', `card${eventsArray[i]._id}`);
         div.innerHTML =
-            `<div class="card-image" id="event${events[i]._id}"></div>  
+            `<div class="card-image" id="bg-event${eventsArray[i]._id}"></div>  
             <div class="card-body">
-                <h5 class="card-title text-center">${events[i].name}</h5>
-                <p class="text-center">${events[i].description}</p>
+                <h5 class="card-title text-center">${eventsArray[i].name}</h5>
+                <p class="text-center">${eventsArray[i].description}</p>
             </div>
             <div class="row card-bottom d-flex align-items-baseline">
                 <div class="col text-center">
-                    <p>Price: $ ${events[i].price}</p>
+                    <p>Price: $ ${eventsArray[i].price}</p>
                 </div>
                 <div class="col btn-details p-0">
                     <a href="#" class="btn btn-sm w-100">Details</a>
                 </div>
             </div>`;
-        document.getElementById("cards").appendChild(div);
-        document.getElementById(`event${events[i]._id}`).style.backgroundImage = `url(${events[i].image})`;
+        dFrag.appendChild(div)
+        dFrag.getElementById(`bg-event${eventsArray[i]._id}`).style.backgroundImage = `url(${eventsArray[i].image})`;
+    }
+    document.getElementById("cards").appendChild(dFrag);
 }
-
 
 
 // Crear un string de objetos para vincular el checkbox con el nombre de la categoría.
@@ -63,55 +55,34 @@ let checkedCategories = [
 
 
 // Cambiar la visibilidad, en función de si el checkbox pertenciente a su categoría está checkeado.
-for (let checkedCategory of checkedCategories) {
-    (checkedCategory.checkbox).addEventListener("change", function() {
-    
-        if (!(checkedCategory.checkbox).checked) {
-            for (let i = 0; i < events.length; i++){
-                if (checkedCategory.category == (events[i].category)) {
-                    document.getElementById(`card${events[i]._id}`).classList.remove('d-flex');
-                    document.getElementById(`card${events[i]._id}`).style.display = `none`;
+function showCards(eventsArray) {
+    for (let checkedCategory of checkedCategories) {
+        (checkedCategory.checkbox).addEventListener("change", function changeDisplay() {
+        
+            if (!(checkedCategory.checkbox).checked) {
+                for (let i = 0; i < eventsArray.length; i++){
+                    if (checkedCategory.category == (eventsArray[i].category)) {
+                        document.getElementById(`card${eventsArray[i]._id}`).classList.remove('d-flex');
+                        document.getElementById(`card${eventsArray[i]._id}`).style.display = `none`;
+                    }
+                }
+            } else {
+                for (let i = 0; i < eventsArray.length; i++){
+                    if (checkedCategory.category == (eventsArray[i].category)) {
+                        document.getElementById(`card${eventsArray[i]._id}`).classList.add('d-flex');
+                        document.getElementById(`card${eventsArray[i]._id}`).removeAttribute('style');
+                    }
                 }
             }
-        } else {
-            for (let i = 0; i < events.length; i++){
-                if (checkedCategory.category == (events[i].category)) {
-                    document.getElementById(`card${events[i]._id}`).classList.add('d-flex');
-                     document.getElementById(`card${events[i]._id}`).removeAttribute('style');
-                }
-            }
-            
-        }
-
-
-
-    // LO QUE ESTÁ ACÁ ABAJO FUE UN MAREO ASTRONÓMICO QUE HACÍA FUNCIONAR AL SITIO MUY LENTO.
-
-    // Armar un array para seguir el funcionamiento de los checkboxes.
-    /* let checkedEvents = [];
-    if ((checkedCategory.checkbox).checked) {
-        checkedEvents.push((checkedCategory.category));
-    } else {
-        checkedEvents.splice((checkedCategory.category), 1);
+        })
     }
-    console.log("checkedEvents: " + checkedEvents) */
-
-    
-    // Mostrar una tarjeta con los datos del evento a partir de los checkboxes seleccionados (comparar nombres de categorías.)
-    /* for (let checkedEvent of checkedEvents) {
-        for (let i = 0; i < events.length; i++){
-        if(checkedEvent == (events[i].category)) {
-            console.log(`card${events[i]._id}`)
-            document.getElementById(`card${events[i]._id}`).classList.remove('d-flex');
-            document.getElementById(`card${events[i]._id}`).style.display = `none`;   
-        } else {
-            document.getElementById(`card${events[i]._id}`).classList.add('d-flex');
-            document.getElementById(`card${events[i]._id}`).removeAttribute('style');
-        }
-    }
-} */
-})
 }
 
 
-   
+// Llamar a la función cardGenerator con el array de eventos completo.
+cardGenerator(events);
+
+
+// Lamar a la función para mostrar tarjetas.
+showCards(events);
+
--- scripts/pastEvents.js
+++ scripts/pastEvents.js
@@ -1,10 +1,13 @@
 import data from "./amazing.js";
 
+
+// Guardar la información de amazing.js en variables para facilitar la manipulación.
 let events = data.events;
 let date = data.currentDate;
 
-// Función que devuelve todos los eventos ANTERIORES a la currentDate
-function pastEvents(){
+
+// Transformar el array de eventos en un array de futuros eventos.
+function pastEvArray(events) {
     let pastEv = [];
     for (let event of events) {
         if (Date.parse(event.date) < Date.parse(date)) {
@@ -13,6 +16,87 @@ function pastEvents(){
     }
     return pastEv;
 }
+console.log(pastEvArray(events));
 
-console.log("La fecha actual es: " +date);
-console.log(pastEvents());
+
+// Recoger los input de los checkboxes de selector de categorías.
+let checkFoodFair = document.getElementById("catFoodFair");
+let checkMuseum = document.getElementById("catMuseum");
+let checkCostumeParty = document.getElementById("catCostumeParty");
+let checkMusicConcert = document.getElementById("catMusicConcert");
+let checkRace = document.getElementById("catRace");
+let checkBookExchange = document.getElementById("catBookExchange");
+let checkCinema = document.getElementById("catCinema");
+
+
+// Crear el molde de tarjeta dentro del div "cards". Asignarle la imagen de fondo.
+function cardGenerator(eventsArray) {
+    let dFrag = document.createDocumentFragment();
+    for (let i = 0; i < eventsArray.length; i++) {
+        let div = document.createElement("div");
+        div.classList.add('card', 'd-flex', 'flex-column');
+        div.setAttribute('id', `card${eventsArray[i]._id}`);
+        div.innerHTML =
+            `<div class="card-image" id="bg-event${eventsArray[i]._id}"></div>  
+            <div class="card-body">
+                <h5 class="card-title text-center">${eventsArray[i].name}</h5>
+                <p class="text-center">${eventsArray[i].description}</p>
+            </div>
+            <div class="row card-bottom d-flex align-items-baseline">
+                <div class="col text-center">
+                    <p>Price: $ ${eventsArray[i].price}</p>
+                </div>
+                <div class="col btn-details p-0">
+                    <a href="#" class="btn btn-sm w-100">Details</a>
+                </div>
+            </div>`;
+        dFrag.appendChild(div)
+        dFrag.getElementById(`bg-event${eventsArray[i]._id}`).style.backgroundImage = `url(${eventsArray[i].image})`;
+    }
+    document.getElementById("cards").appendChild(dFrag);
+}
+
+
+// Crear un string de objetos para vincular el checkbox con el nombre de la categoría.
+let checkedCategories = [
+    {checkbox: checkFoodFair, category: "Food Fair"},
+    {checkbox: checkMuseum, category: "Museum"},
+    {checkbox: checkCostumeParty, category: "Costume Party"},
+    {checkbox: checkMusicConcert, category: "Music Concert"},
+    {checkbox: checkRace, category: "Race"},
+    {checkbox: checkBookExchange, category: "Book Exchange"},
+    {checkbox: checkCinema, category: "Cinema"}]
+
+
+// Cambiar la visibilidad, en función de si el checkbox pertenciente a su categoría está checkeado.
+function showCards(eventsArray) {
+    for (let checkedCategory of checkedCategories) {
+        (checkedCategory.checkbox).addEventListener("change", function changeDisplay() {
+        
+            if (!(checkedCategory.checkbox).checked) {
+                for (let i = 0; i < eventsArray.length; i++){
+                    if (checkedCategory.category == (eventsArray[i].category)) {
+                        document.getElementById(`card${eventsArray[i]._id}`).classList.remove('d-flex');
+                        document.getElementById(`card${eventsArray[i]._id}`).style.display = `none`;
+                    }
+                }
+            } else {
+                for (let i = 0; i < eventsArray.length; i++){
+                    if (checkedCategory.category == (eventsArray[i].category)) {
+                        document.getElementById(`card${eventsArray[i]._id}`).classList.add('d-flex');
+                        document.getElementById(`card${eventsArray[i]._id}`).removeAttribute('style');
+                    }
+                }
+            }
+        })
+    }
+}
+
+
+// Lamar a la función para generar tarjetas.
+cardGenerator(pastEvArray(events));
+
+
+// Lamar a la función para mostrar tarjetas.
+showCards(pastEvArray(events));
+
